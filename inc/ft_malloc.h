131cf376dcd150d16b8327760fa6a570f0bd8c90
moulin-louis <louis.moulin@outlook.fr>
2023-12-05 13:18:57 +0100

    little step malloc

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -1,6 +1,12 @@
 #ifndef FT_MALLOC_LIBRARY_H
 #define FT_MALLOC_LIBRARY_H
 
+#include <sys/types.h>
+
+void   *malloc(size_t size);
+void   *realloc(void *ptr, size_t size);
+void   free(void *ptr);
+
 void hello(void);
 
 #endif //FT_MALLOC_LIBRARY_H
